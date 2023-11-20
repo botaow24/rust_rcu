131d6782507071a3d1743294c20ad949e8c89c75
Botao Wu <botaow24@outlook.com>
2023-11-19 23:53:08 -0500

    feat: first version

--- src/RcuGP.rs
+++ src/RcuGP.rs
@@ -1,70 +1,94 @@
-
 use std::cell::UnsafeCell;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::ptr::NonNull;
+use std::sync::atomic::{AtomicI32, AtomicU32, Ordering, AtomicPtr};
 use std::sync::{Arc, RwLock};
-use std::sync::atomic::{AtomicU32,AtomicI32, Ordering};
 
-use std::sync::{Mutex};
+use std::sync::Mutex;
 
+pub struct RcuGPShared<T> {
+    thread_counter: AtomicU32,
 
-
-pub struct RcuGPShared <T: ?Sized>
-{
-    thread_counter:AtomicU32,
-
-    global_ctr : AtomicU32,
+    global_ctr: AtomicU32,
 
     thread_ctr: Vec<AtomicU32>,
 
     mtx: Mutex<i32>,
 
-    data: UnsafeCell<T>,
-
+    data_ptr : AtomicPtr<T>,
+    data: Mutex<Box<UnsafeCell<T>>>,
 }
 
-fn barrier()
-{
-
-}
+fn barrier() {}
 
 const RCU_NEST_MASK: u32 = 0x0ffff;
 const RCU_GP_CTR_PHASE: u32 = 0x10000;
-const RCU_NEST_COUNT: u32 = 0x1 ;
+const RCU_NEST_COUNT: u32 = 0x1;
 
-fn smp_mb()
-{
+fn smp_mb() {}
 
-}
-
-fn is_busy(ctr : & AtomicU32, global_ctr:u32) ->bool
-{
+fn is_busy(ctr: &AtomicU32, global_ctr: u32) -> bool {
     let value = ctr.load(Ordering::Relaxed);
     return (value & RCU_NEST_MASK) != 0 && ((value ^ global_ctr) & RCU_GP_CTR_PHASE) != 0;
 }
 
-impl<T> RcuGPShared <T>
-{
-    pub fn new(count:i32,data:T) ->Self{
+impl<T> RcuGPShared<T> {
+    pub fn new(count: i32, data: T) -> Self {
         let mut my_vec = Vec::new();
-        for r in 0..count
-        {
+        for r in 0..count {
             my_vec.push(AtomicU32::new(0));
-        } 
-        return RcuGPShared{
-            thread_counter:AtomicU32::new(0),
-            global_ctr:AtomicU32::new(0),
-            thread_ctr:my_vec,
-            mtx:Mutex::new(0),
-            data:data.into()
         }
+        let mut bx : Box<UnsafeCell<T>> = Box::new(data.into());
+        return RcuGPShared {
+            thread_counter: AtomicU32::new(0),
+            global_ctr: AtomicU32::new(0),
+            thread_ctr: my_vec,
+            mtx: Mutex::new(0),
+            data_ptr: AtomicPtr:: new(bx.as_mut().get_mut()) ,
+            data: Mutex::new(bx),
+        };
     }
 }
 
-unsafe impl <T> Send for RcuGPShared <T> {}
-unsafe impl <T> Sync for RcuGPShared <T> {}
 
-pub struct RcuGpReadGuard<'a, T: ?Sized + 'a> {
+unsafe impl<T> Send for RcuGPShared<T> {}
+unsafe impl<T> Sync for RcuGPShared<T> {}
+
+
+pub struct RcuGpWriteGuard<'a, T: 'a> {
+    // NB: we use a pointer instead of `&'a T` to avoid `noalias` violations, because a
+    // `Ref` argument doesn't hold immutability for its whole scope, only until it drops.
+    // `NonNull` is also covariant over `T`, just like we would have with `&T`. `NonNull`
+    // is preferable over `const* T` to allow for niche optimization.
+    inner_lock: &'a RcuGPLock<T>,
+    data: Box<UnsafeCell<T>>,
+}
+
+
+
+impl<'a, T: 'a> RcuGpWriteGuard<'a, T> {
+    pub fn new(lock: &'a RcuGPLock<T>, mut new_data: T) -> Self {
+
+        lock.global_info.data_ptr.store(& mut new_data, Ordering::Release);
+        let mut mtx = lock.global_info.data.lock().unwrap();
+        let mut bx : Box<UnsafeCell<T>> = Box::new(new_data.into());
+        let old = std::mem::replace(&mut *mtx,bx);
+        return RcuGpWriteGuard {
+            inner_lock: lock,
+            data: old
+        };
+    }
+}
+
+impl<'a, T> Drop for RcuGpWriteGuard<'a, T> {
+    fn drop(&mut self) {
+        self.inner_lock.synchronize_rcu();
+    }
+}
+
+
+
+pub struct RcuGpReadGuard<'a, T:  'a> {
     // NB: we use a pointer instead of `&'a T` to avoid `noalias` violations, because a
     // `Ref` argument doesn't hold immutability for its whole scope, only until it drops.
     // `NonNull` is also covariant over `T`, just like we would have with `&T`. `NonNull`
@@ -73,17 +97,19 @@ pub struct RcuGpReadGuard<'a, T: ?Sized + 'a> {
     inner_lock: &'a RcuGPLock<T>,
 }
 
-impl <'a, T: ?Sized + 'a> RcuGpReadGuard<'a, T> 
-{
-    pub fn new(lock:&'a RcuGPLock<T>) -> Self {
-    
-        return RcuGpReadGuard{
-            data:unsafe { NonNull::new_unchecked(lock.global_info.data.get()) },
-            inner_lock:lock};
+
+
+
+impl<'a, T: 'a> RcuGpReadGuard<'a, T> {
+    pub fn new(lock: &'a RcuGPLock<T>) -> Self {
+        return RcuGpReadGuard {
+            data: unsafe { NonNull::new_unchecked(lock.global_info.data_ptr.load(Ordering::Acquire)) },
+            inner_lock: lock,
+        };
     }
 }
 
-impl<T: ?Sized> Deref for RcuGpReadGuard<'_, T> {
+impl<T> Deref for RcuGpReadGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
@@ -92,74 +118,71 @@ impl<T: ?Sized> Deref for RcuGpReadGuard<'_, T> {
     }
 }
 
-impl<'a,T:?Sized> Drop for RcuGpReadGuard<'a,T>
-{
-fn drop(&mut self)
-{
-    self.inner_lock.read_unlock();
-}
+impl<'a, T> Drop for RcuGpReadGuard<'a, T> {
+    fn drop(&mut self) {
+        self.inner_lock.read_unlock();
+    }
 }
 
-pub struct RcuGPLock <T: ?Sized>
-{
-    dammy : AtomicU32,
+pub struct RcuGPLock<T> {
+    dammy: AtomicU32,
 
-    thread_id : usize,
+    thread_id: usize,
 
-    global_info :Arc<RcuGPShared<T>>,
+    global_info: Arc<RcuGPShared<T>>,
 }
 
-
-
-impl <T: ?Sized> RcuGPLock<T> {
-
-    pub fn new(shared:Arc<RcuGPShared<T>>) -> Self {
+impl<T> RcuGPLock<T> {
+    pub fn new(shared: Arc<RcuGPShared<T>>) -> Self {
         let tc = shared.thread_counter.fetch_add(1, Ordering::SeqCst);
-        
-        return RcuGPLock{  
-            dammy:AtomicU32::new(0),
+
+        return RcuGPLock {
+            dammy: AtomicU32::new(0),
             thread_id: tc as usize,
-            global_info:shared}
+            global_info: shared,
+        };
     }
 
-    pub fn read(&self) -> RcuGpReadGuard<'_, T>
-    {
+    pub fn read(&self) -> RcuGpReadGuard<'_, T> {
         self.read_lock();
 
         return RcuGpReadGuard::new(self);
     }
 
-    fn read_lock(&self) 
+    pub fn replace(&self, new_data:T) -> RcuGpWriteGuard<'_, T>
     {
+        
+        return RcuGpWriteGuard::new(self,new_data);
+    }
+
+    fn read_lock(&self) {
         //println!("read");
         let id = self.thread_id;
         let temp_local = self.global_info.thread_ctr[id].load(Ordering::Acquire);
-        if (temp_local & RCU_NEST_MASK)==0
-        {
-            self.global_info.thread_ctr[id].store(self.global_info.global_ctr.load(Ordering::Acquire) + RCU_NEST_COUNT,Ordering::SeqCst );
+        if (temp_local & RCU_NEST_MASK) == 0 {
+            self.global_info.thread_ctr[id].store(
+                self.global_info.global_ctr.load(Ordering::Acquire) + RCU_NEST_COUNT,
+                Ordering::SeqCst,
+            );
             smp_mb();
-        }
-        else 
-        {
+        } else {
             self.global_info.thread_ctr[id].store(temp_local + RCU_NEST_COUNT, Ordering::Relaxed)
             //rlocal_ctr[id].store(global_ctr.read(Ordering::Acquire),Ordering::Release );
         }
     }
 
-    fn read_unlock(&self) 
-    {
+    fn read_unlock(&self) {
         //println!("read unlock");
         smp_mb();
         let id = self.thread_id;
         let temp_local = self.global_info.thread_ctr[id].load(Ordering::Acquire);
-        self.global_info.thread_ctr[id].store(temp_local- RCU_NEST_COUNT, Ordering::SeqCst)
+        self.global_info.thread_ctr[id].store(temp_local - RCU_NEST_COUNT, Ordering::SeqCst)
     }
 
-    fn synchronize_rcu(&self)
-    {
+    fn synchronize_rcu(&self) {
         smp_mb();
         {
-            let _lg = self.global_info.mtx.lock().unwrap() ;
+            let _lg = self.global_info.mtx.lock().unwrap();
             self.update_counter_and_wait();
             barrier();
             self.update_counter_and_wait();
@@ -167,21 +190,17 @@ impl <T: ?Sized> RcuGPLock<T> {
         smp_mb();
     }
 
-    fn update_counter_and_wait(&self)
-    {   
+    fn update_counter_and_wait(&self) {
         let old_value: u32 = self.global_info.global_ctr.load(Ordering::Acquire);
-        let new_value : u32 = old_value ^ RCU_GP_CTR_PHASE;
-        self.global_info.global_ctr.store(new_value ^ RCU_GP_CTR_PHASE,Ordering::Release );
+        let new_value: u32 = old_value ^ RCU_GP_CTR_PHASE;
+        self.global_info
+            .global_ctr
+            .store(new_value ^ RCU_GP_CTR_PHASE, Ordering::Release);
         barrier();
-        for ctr in &self.global_info.thread_ctr
-        {
-            while is_busy(ctr,new_value)
-            {
+        for ctr in &self.global_info.thread_ctr {
+            while is_busy(ctr, new_value) {
                 std::thread::yield_now();
             }
         }
     }
-
-
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 
 use std::num::Wrapping;
+
 //use std::io;
 //use rand::Rng;
 //use std::cmp::Ordering;
-use std::thread;
+use std::{thread, mem, time};
 //use std::time::Duration;
-use std::sync::atomic::{AtomicU32,AtomicI32, Ordering};
+use std::sync::atomic::{AtomicI32, AtomicU32, Ordering};
 //use std::ptr::{self, null_mut};
-use std::sync::{Arc, RwLock, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::time::Instant;
 
 use rand::Rng;
@@ -15,97 +16,110 @@ use rand::Rng;
 mod RcuGP;
 //use rand::distributions::Uniform;
 
+static N_THREADS: u32 = 8;
 
+struct Node {
+    id: AtomicI32,
+    accept: AtomicU32,
+    reject: AtomicU32,
 
-static N_THREADS:u32 = 8;
-
-
-
-struct Node
-{    
-    id:AtomicI32,
-    accept:AtomicU32,
-    reject:AtomicU32,
-   
-    payload:Vec<u32> ,
+    payload: Vec<u32>,
 }
 
-struct World{
-    //node: * mut Node,
-    node:Node,
-    //user:AtomicU32,
-}
-
-fn gen_node() -> Node
-{
+fn gen_node() -> Node {
     static mut GID: i32 = 0;
     let mut rng = rand::thread_rng();
     let vals: Vec<u32> = (0..512).map(|_| rng.gen_range(1..512)).collect();
     unsafe { GID += 1 };
-    let n = Node{id : AtomicI32::new(unsafe { GID }) ,accept :AtomicU32::new(0),reject:AtomicU32::new(0),payload :vals};
+    let n = Node {
+        id: AtomicI32::new(unsafe { GID }),
+        accept: AtomicU32::new(0),
+        reject: AtomicU32::new(0),
+        payload: vals,
+    };
     return n;
 }
 
-
-
-fn thread_checker(world: RcuGP::RcuGPLock<World>,id:u32){
-    println!("checker Start id #{}",id);
+fn thread_checker(world: RcuGP::RcuGPLock<Node>, id: u32) {
+    println!("checker Start id #{}", id);
     let mut last_value: i32 = -1;
-    loop{
+    loop {
         let guard = world.read();
-        let now_id = guard.node.id.load(Ordering::Acquire);
-    if last_value == now_id
-    {
-        if guard.node.accept.load(Ordering::Acquire) == N_THREADS
-            {
+        let now_id = guard.id.load(Ordering::Acquire);
+        if last_value == now_id {
+            if guard.accept.load(Ordering::Acquire) == N_THREADS {
                 break;
             }
-    }
-    else 
-    {
-        last_value = now_id;
-        let mut valid:bool = true;
-        let mut idx = 0;
-        
-        for value in &guard.node.payload
-        {
-            if id == *value
-            {
-                println!("thread {} reject Node{} @ {}",id,now_id,idx);
-                valid = false;
-                break;
+        } else {
+            last_value = now_id;
+            let mut valid: bool = true;
+            let mut idx = 0;
+
+            //thread::sleep(time::Duration::from_millis(1000));
+
+            for value in &guard.payload {
+                if id == *value {
+                    println!("thread {} reject Node{} @ {}", id, now_id, idx);
+                    valid = false;
+                    break;
+                }
+                idx += 1;
             }
-            idx += 1;
+
+            if valid {
+                println!("thread {} accept Node{}", id, now_id);
+                guard.accept.fetch_add(1, Ordering::AcqRel);
+            } else {
+                guard.reject.fetch_add(1, Ordering::AcqRel);
+            }
         }
-         
-        if valid
-        {
-            println!("thread {} accept Node{}",id,now_id);
-            guard.node.accept.fetch_add(1, Ordering::AcqRel);
-        }
-        else 
-        {
-            guard.node.reject.fetch_add(1, Ordering::AcqRel);
-        }
-         
     }
-    }   
+    println!("checker Exit id #{}", id);
+}
+
+fn thread_creator(_world: RcuGP::RcuGPLock<Node>) {
+    println!("creator Start");
+
+    loop {
+        if _world.read().reject.load(Ordering::Acquire) != 0 {
+            let new_node = gen_node();
+            println!("Creating new Block{}", new_node.id.load(Ordering::Relaxed));
+            _world.replace(new_node);
+        } else if _world.read().accept.load(Ordering::Acquire) == N_THREADS {
+            break;
+        }
+    }
+
+    println!("creator Exit");
 }
 
 fn testGP() {
     let now = Instant::now();
-    let world: World= World{node:gen_node()};
-    let shared = Arc::new(RcuGP::RcuGPShared::new((N_THREADS+1).try_into().unwrap(),world));
-    let mut handles = vec![];
-    for id in [2, 3, 5, 7, 11, 13, 17, 19] {      
+    let node: Node  =  gen_node() ;
+    let shared: Arc<RcuGP::RcuGPShared<Node>> = Arc::new(RcuGP::RcuGPShared::new(
+        (N_THREADS + 1).try_into().unwrap(),
+        node,
+    ));
+    println!("{}  {}   {}" ,mem::size_of::<Node>(),mem::size_of::<RcuGP::RcuGPShared<Node>>(),mem::size_of::<RcuGP::RcuGPShared<u32>>());
 
+    let mut handles = vec![];
+    for id in [2, 3, 5, 7, 11, 13, 17, 19] {
         let wc = RcuGP::RcuGPLock::new(shared.clone());
-        
-        let handle = thread::spawn( move || {thread_checker(wc,id);});
+
+        let handle = thread::spawn(move || {
+            thread_checker(wc, id);
+        });
         handles.push(handle);
-        
     }
- 
+
+    {
+        let wc = RcuGP::RcuGPLock::new(shared.clone());
+        let handle = thread::spawn(move || {
+            thread_creator(wc);
+        });
+        handles.push(handle);
+    }
+
     for handle in handles {
         handle.join().unwrap();
     }
@@ -114,7 +128,9 @@ fn testGP() {
     println!("Exit ");
 }
 
-
 fn main() {
+    loop
+    {
     testGP();
+    }
 }
